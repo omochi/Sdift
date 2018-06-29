65baee006be64cb0e14ae441eb3649077500e14e
omochimetaru <omochi.metaru@gmail.com>
2018-06-29 14:39:44 +0900

    debugger friendly code

--- Sources/Sdift/Algorithm.swift
+++ Sources/Sdift/Algorithm.swift
@@ -27,11 +27,29 @@ public func difference<OC: Collection, NC: Collection>(
 public func difference<O, N>(
     old: [O],
     new: [N],
-    equals equalsImpl: @escaping (O, N) -> Bool) -> Difference
+    equals: @escaping (O, N) -> Bool) -> Difference
 {
-    var equalCache: [CacheKey: Bool] = [:]
+    var solver = Solver(old: old, new: new, equals: equals)
+    return solver.difference()
+}
+
+internal struct Solver<O, N> {
+    private let old: [O]
+    private let new: [N]
+    private let equalsImpl: (O, N) -> Bool
     
-    func rawEquals(oldIndex: Int, newIndex: Int) -> Bool {
+    public init(old: [O],
+                new: [N],
+                equals equalsImpl: @escaping (O, N) -> Bool)
+    {
+        self.old = old
+        self.new = new
+        self.equalsImpl = equalsImpl
+    }
+    
+    private var equalCache: [CacheKey: Bool] = [:]
+    
+    private mutating func rawEquals(oldIndex: Int, newIndex: Int) -> Bool {
         let cacheKey = CacheKey(oldIndex: oldIndex, newIndex: newIndex)
         if let cache = equalCache[cacheKey] {
             return cache
@@ -44,7 +62,18 @@ public func difference<O, N>(
         return result
     }
     
-    func recurse(old: Slice<O>, new: Slice<N>) -> [Difference.Item] {
+    public mutating func difference() -> Difference {
+        let oldSlice = Slice(elements: old,
+                             offset: 0,
+                             count: old.count)
+        let newSlice = Slice(elements: new,
+                             offset: 0,
+                             count: new.count)
+        let diffItems = self.difference(old: oldSlice, new: newSlice)
+        return  Difference(diffItems)
+    }
+    
+    private mutating func difference(old: Slice<O>, new: Slice<N>) -> [Difference.Item] {
         func equals(oldIndex: Int, newIndex: Int) -> Bool {
             return rawEquals(oldIndex: old.offset + oldIndex,
                              newIndex: new.offset + newIndex)
@@ -77,45 +106,39 @@ public func difference<O, N>(
         let stepNum: Int = (maxDistance + 1) / 2 + 1
         
         for step in 0..<stepNum {
-            let minK: Int = {
-                if step <= new.count {
-                    return -step
-                } else {
-                    return -(new.count - (step - new.count))
-                }
-            }()
+            let minK: Int
+            if step <= new.count {
+                minK = -step
+            } else {
+                minK = -(new.count - (step - new.count))
+            }
             
-            let maxK: Int = {
-                if step <= old.count {
-                    return step
-                } else {
-                    return old.count - (step - old.count)
-                }
-            }()
+            let maxK: Int
+            if step <= old.count {
+                maxK = step
+            } else {
+                maxK = old.count - (step - old.count)
+            }
             
             for forwardK in stride(from: minK, through: maxK, by: 2) {
-                let direction: ForwardDirection = {
-                    if forwardK == -step {
-                        return .down
-                    }
-                    if forwardK == step {
-                        return .right
-                    }
-                    if forwardTable[k: forwardK - 1] < forwardTable[k: forwardK + 1] {
-                        return .down
-                    } else {
-                        return .right
-                    }
-                }()
+                let direction: ForwardDirection
+                if forwardK == -step {
+                    direction = .down
+                } else if forwardK == step {
+                    direction = .right
+                } else if forwardTable[k: forwardK - 1] < forwardTable[k: forwardK + 1] {
+                    direction = .down
+                } else {
+                    direction = .right
+                }
                 
-                let snakeStartX: Int = {
-                    switch direction {
-                    case .down:
-                        return forwardTable[k: forwardK + 1]
-                    case .right:
-                        return forwardTable[k: forwardK - 1] + 1
-                    }
-                }()
+                let snakeStartX: Int
+                switch direction {
+                case .down:
+                    snakeStartX = forwardTable[k: forwardK + 1]
+                case .right:
+                    snakeStartX = forwardTable[k: forwardK - 1] + 1
+                }
                 let snakeStartY: Int = snakeStartX - forwardK
                 
                 var snakeEndX = snakeStartX
@@ -138,35 +161,31 @@ public func difference<O, N>(
                     if -(step-1) <= backwardK, backwardK <= (step-1),
                         forwardTable[k: forwardK] + backwardTable[k: backwardK] >= old.count
                     {
-                        return recurse(old: old[0..<snakeStartX], new: new[0..<snakeStartY]) +
-                            recurse(old: old[snakeEndX..<old.count], new: new[snakeEndY..<new.count])
+                        return difference(old: old[0..<snakeStartX], new: new[0..<snakeStartY]) +
+                            difference(old: old[snakeEndX..<old.count], new: new[snakeEndY..<new.count])
                     }
                 }
             }
             
             for backwardK in stride(from: minK, through: maxK, by: 2) {
-                let direction: BackwardDirection = {
-                    if backwardK == -step {
-                        return .up
-                    }
-                    if backwardK == step {
-                        return .left
-                    }
-                    if backwardTable[k: backwardK - 1] < backwardTable[k: backwardK + 1] {
-                        return .up
-                    } else {
-                        return .left
-                    }
-                }()
+                let direction: BackwardDirection
+                if backwardK == -step {
+                    direction = .up
+                } else if backwardK == step {
+                    direction = .left
+                } else if backwardTable[k: backwardK - 1] < backwardTable[k: backwardK + 1] {
+                    direction = .up
+                } else {
+                    direction = .left
+                }
                 
-                let snakeStartX: Int = {
-                    switch direction {
-                    case .up:
-                        return backwardTable[k: backwardK + 1]
-                    case .left:
-                        return backwardTable[k: backwardK - 1] + 1
-                    }
-                }()
+                let snakeStartX: Int
+                switch direction {
+                case .up:
+                    snakeStartX = backwardTable[k: backwardK + 1]
+                case .left:
+                    snakeStartX = backwardTable[k: backwardK - 1] + 1
+                }
                 let snakeStartY: Int = snakeStartX - backwardK
                 
                 var snakeEndX = snakeStartX
@@ -190,9 +209,9 @@ public func difference<O, N>(
                     if -step <= forwardK, forwardK <= step,
                         forwardTable[k: forwardK] + backwardTable[k: backwardK] >= old.count
                     {
-                        return recurse(old: old[0..<(old.count - snakeEndX)],
+                        return difference(old: old[0..<(old.count - snakeEndX)],
                                           new: new[0..<(new.count - snakeEndY)]) +
-                            recurse(old: old[(old.count - snakeStartX)..<old.count],
+                            difference(old: old[(old.count - snakeStartX)..<old.count],
                                        new: new[(new.count - snakeStartY)..<new.count])
                     }
                 }
@@ -201,12 +220,4 @@ public func difference<O, N>(
         
         fatalError("never reach here")
     }
-    
-    return Difference(recurse(old: Slice(elements: old,
-                                         offset: 0,
-                                         count: old.count),
-                              new: Slice(elements: new,
-                                         offset: 0,
-                                         count: new.count)))
 }
-
