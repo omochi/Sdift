0177227e59f35026fb5d8a376fd73d45f3301265
omochimetaru <omochi.metaru@gmail.com>
2018-06-29 12:52:16 +0900

    Carthage

---
+++ Resources/Sdift/Sdift.h
@@ -0,0 +1,19 @@
+//
+//  Sdift.h
+//  Sdift
+//
+//  Created by omochimetaru on 2018/06/29.
+//  Copyright © 2018年 omochimetaru.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sdift.
+FOUNDATION_EXPORT double SdiftVersionNumber;
+
+//! Project version string for Sdift.
+FOUNDATION_EXPORT const unsigned char SdiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sdift/PublicHeader.h>
+
+
